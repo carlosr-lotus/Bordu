779011c34e406a8fb0ebd33be66bb01af78d7c21
Carlos R Lotus <carlosr_juniorpro@protonmail.com>
2021-07-11 21:44:20 -0300

    App Main React File

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 // Import ONLY the components here
 import Container from "./components/Container";
 import Header from "./components/Header";
+import Carousel from "./components/Carousel";
 
 function App() {
   return (
     <div className="App">
       <Container>
         <Header />
+        <Carousel />
       </Container>
     </div>
   );
