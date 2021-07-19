75879109b79e62ce86cc0958bfe50ef42e576bdb
Carlos R Lotus <carlosr_juniorpro@protonmail.com>
2021-07-18 21:26:02 -0300

    Our Products

---
+++ src/components/OurProducts/index.js
@@ -0,0 +1,9 @@
+import './OurProducts.css';
+
+export default function OurProducts() {
+    return (
+        <div className="OurProducts-container">
+            <h2>Nossos Produtos Exemplo</h2>
+        </div>
+    )
+};
