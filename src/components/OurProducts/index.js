b2684dfd795ae50dbee2b0ba2bf695e384ec93e1
Carlos R Lotus <carlosr_juniorpro@protonmail.com>
2021-07-21 21:32:15 -0300

    Our Products

--- src/components/OurProducts/index.js
+++ src/components/OurProducts/index.js
@@ -6,17 +6,17 @@ import './OurProducts.css';
 
     /* !!!!! 
      TASKS TO COMPLETE:
-        1) FIX MOBILE NAV BAR TO WORK PROPERLY WITH USECONTEXT()
-            1.1) Create 'MAIN' component to englobe all components in the APP besides Header and Footer
-            1.2) Make sure when mobile navbar is TRUE, all the content inside 'MAIN' component is gone so it can be responsive.
-        2) ADJUST TO BE RESPONSIVE IN DIFFERENT RESOLUTIONS = pending... 
-        3) CHECK IF THE CAROUSEL/SLIDER WORKS PROPERLY = pending...
+        1) FIX MOBILE NAV BAR TO WORK PROPERLY WITH USECONTEXT() = COMPLETED with adding position: fixed in the mobile menu bar
+        2) ADJUST TO BE RESPONSIVE IN DIFFERENT RESOLUTIONS = COMPLETED
+        3) CREATE 'OURPRODUCTS' SECTION AND MAKE IT RESPONSIVE ACCORDING TO THE DESIGN PROVIDED = PENDING...
      * YOU ARE AMAZING! AND YOU WILL FINISH THIS WEBSITE AND BECOME AN AMAZING DEVELOPER
      * TRUST IN YOURSELF! YOU CAN DO IT!!!
      !!!!!
     */
 
 export default function OurProducts() {
+
+
     return (
         <div className="OurProducts-container">
             <h1>Nossos Produtos</h1>
@@ -26,15 +26,37 @@ export default function OurProducts() {
 
                 <div className="OurProducts-img-container">
                     <img src={require("../../images/our-products-1.jpg").default} alt="makeup" />
+                    <div className="red-ball"></div>
                 </div>
 
-                <h2>Maquiagem</h2>
-                <p>Nós trabalhamos duro para oferecer aos nossos clientes a experiência de uma beleza única e memorável. Seja belo(a) com produtos totalmente orgânicos!</p>
+                <div className="OurProducts-box-content">
+                    <h2>Maquiagem</h2>
+                    <p>Nós trabalhamos duro para oferecer aos nossos clientes a experiência de uma beleza única e memorável. Seja belo(a) com produtos totalmente orgânicos!</p>
 
-                <button className="red-button">
-                    <FaShoppingBag />
-                    Comprar
-                </button>
+                    <button className="red-button">
+                        <FaShoppingBag />
+                        <p>Comprar</p>
+                    </button>
+                </div>
+
+            </div>
+
+            {/* This div will contain details imported from 'OurProductsData' */}
+            <div className="OurProducts-local-container">
+
+                <div className="OurProducts-img-container">
+                    <img src={require("../../images/our-products-2.jpeg").default} alt="makeup" />
+                </div>
+
+                <div className="OurProducts-box-content justify-right">
+                    <h2>Fragrâncias</h2>
+                    <p>Tem algo mais charmoso ao sentir o gostoso cheiro de um perfume? Seria melhor ainda vindo de nós. Temos diversos perfumes perfeitos para qualquer ocasião.</p>
+
+                    <button className="red-button">
+                        <FaShoppingBag />
+                        <p>Comprar</p>
+                    </button>
+                </div>
 
             </div>
         </div>
