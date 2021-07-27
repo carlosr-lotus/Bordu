61f1c99355eedcdb423122895ef222ba4c4e3baf
Carlos R Lotus <carlosr_juniorpro@protonmail.com>
2021-07-27 15:09:29 -0300

    Added Pages Folder

---
+++ src/pages/Homepage/index.js
@@ -0,0 +1,17 @@
+import Carousel from "../../components/Carousel";
+import OurProducts from "../../components/OurProducts";
+import AboutUs from "../../components/AboutUs";
+import Author from "../../components/Author";
+import Footer from "../../components/Footer";
+
+export default function Homepage() {
+    return (
+        <>
+            <Carousel />
+            <OurProducts />
+            <AboutUs />
+            <Author />
+            <Footer />
+        </>
+    )
+};
