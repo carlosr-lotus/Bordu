1bee535376039b31492dd1d12bb94ffc351e080a
Carlos R Lotus <carlosr_juniorpro@protonmail.com>
2021-07-27 15:14:54 -0300

    Added Switch and Route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-// Import ONLY the components here
 import Container from "./components/Container";
 import Header from "./components/Header";
-import Carousel from "./components/Carousel";
-import OurProducts from "./components/OurProducts";
+import Homepage from "./pages/Homepage";
+
+import {Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Container>
         <Header />
-        <Carousel />
-        <OurProducts />
+
+        <Switch>
+          <Route path="/" exact component={Homepage}/>
+        </Switch>
+        
       </Container>
     </div>
   );
