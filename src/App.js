6e6314a9b83af09fccb59085dfa194daeeaa238e
Carlos R Lotus <carlosr_juniorpro@protonmail.com>
2021-08-01 01:26:42 -0300

    App Main File

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import Container from "./components/Container";
 import Header from "./components/Header";
 import Homepage from "./pages/Homepage";
+import CareerPage from "./pages/Careers";
 
-import {Switch, Route} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
 
         <Switch>
           <Route path="/" exact component={Homepage}/>
+          <Route path="/careers" component={CareerPage} />
         </Switch>
         
       </Container>
