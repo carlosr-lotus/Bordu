a7df779fa4f112ebc66801ef4f767dd2f28570a6
Carlos R Lotus <carlosr_juniorpro@protonmail.com>
2021-08-02 13:49:53 -0300

    Makeup Shop Page

---
+++ src/pages/MakeupShop/index.js
@@ -0,0 +1,11 @@
+import MakeupShop from "../../components/MakeupShop";
+import Footer from "../../components/Footer";
+
+export default function MakeupShopPage() {
+    return (
+        <>
+            <MakeupShop />
+            <Footer />
+        </>
+    )
+}
