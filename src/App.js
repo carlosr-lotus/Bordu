743e9e2690d68cbcc02e92f9ef381b94122a67b6
Carlos R Lotus <carlosr_juniorpro@protonmail.com>
2021-08-06 16:21:03 -0300

    Adjust Router

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import Container from "./components/Container";
 import Header from "./components/Header";
 import Homepage from "./pages/Homepage";
 import MakeupShopPage from "./pages/MakeupShop";
+import MakeupProductPage from "./pages/MakeupShop/MakeupProduct";
+import FragranceShopPage from "./pages/FragranceShop";
 import CareerPage from "./pages/Careers";
 
 import { Switch, Route } from 'react-router-dom';
+
 
 function App() {
   return (
@@ -14,7 +17,12 @@ function App() {
 
         <Switch>
           <Route path="/" exact component={Homepage}/>
-          <Route path="/makeup" component={MakeupShopPage} />
+          <Route path="/fragrance" component={FragranceShopPage} />
+
+          <Route path="/makeup" exact component={MakeupShopPage} />
+          {/* The page below is to the render the component with the id from the product choosen */}
+          <Route path="/makeup/product" component={MakeupProductPage}/>
+
           <Route path="/careers" component={CareerPage} />
         </Switch>
         
