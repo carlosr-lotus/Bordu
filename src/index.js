3b4c65ddf2118f7eea095d090c949900483a10d1
Carlos R Lotus <carlosr_juniorpro@protonmail.com>
2021-08-06 16:23:15 -0300

    Added Redux

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
+// React Tools + Global CSS File
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 import './global.css'
 
+// Components
+import ScrollToTop from './ScrollToTop';
+import reportWebVitals from './reportWebVitals';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <ScrollToTop />
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
