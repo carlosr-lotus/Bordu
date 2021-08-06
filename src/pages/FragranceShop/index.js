82b438045154c936c772cd6dd0ce880eecba1739
Carlos R Lotus <carlosr_juniorpro@protonmail.com>
2021-08-06 16:34:16 -0300

    Fragrance Shop Page

---
+++ src/pages/FragranceShop/index.js
@@ -0,0 +1,11 @@
+import FragranceShop from "../../components/FragranceShop";
+import Footer from "../../components/Footer";
+
+export default function FragranceShopPage() {
+    return (
+        <>
+            <FragranceShop />
+            <Footer />
+        </>
+    )
+}
