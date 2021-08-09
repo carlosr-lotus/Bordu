6ae0ba74f6796bca9e7e53a5452ebdfdf9d80e56
Carlos R Lotus <carlosr_juniorpro@protonmail.com>
2021-08-09 20:41:55 -0300

    Makeup Product Page

--- src/pages/MakeupShop/MakeupProduct/index.js
+++ src/pages/MakeupShop/MakeupProduct/index.js
@@ -1,16 +1,21 @@
 import MakeupProduct from "../../../components/MakeupShop/MakeupProduct";
+import MakeupProductReviews from "../../../components/MakeupShop/MakeupProduct/MakeupProductReviews";
 
 import { useSelector } from "react-redux";
 
+
 export default function MakeupProductPage() {
 
-    const reducer = useSelector(state => state.productPage)
-    console.log(reducer.id);
+    const reducer = useSelector(state => state.productPage);
+    console.log(reducer.reviews);
 
     return (
         <>
-            {/* Call component and pass the reducer containing the data do display the properly product */}
+            {/* Call component and pass the reducer containing the data object to display the properly product */}
             {reducer ? <MakeupProduct data={reducer}/> : <p>Error 404</p>}
+
+            {/* Call component and pass the object containing the reviews that is inside the product object */}
+            <MakeupProductReviews data={reducer.reviews}/>
         </>
     )
-}
+};
