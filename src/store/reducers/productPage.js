b6b23425603bc3140ef77132fe2d9f0dd7832275
Carlos R Lotus <carlosr_juniorpro@protonmail.com>
2021-08-11 11:23:03 -0300

    PProduct Page Reducer

--- src/store/reducers/productPage.js
+++ src/store/reducers/productPage.js
@@ -1,5 +1,6 @@
 const INITIAL_STATE = JSON.parse(localStorage.getItem("product"));
 
+// This reducer will be responsible get the object data containing the product details (example: "MakeupShopData") and return it to the product page container
 export default function productPage(state = INITIAL_STATE, action) {
     switch (action.type) {
         case "SELECT_PRODUCT": 
