5ffe47632a5881ad0c1c325650e4b613fdf742eb
Carlos R Lotus <carlosr_juniorpro@protonmail.com>
2021-08-11 11:16:08 -0300

    Added Fragrance Link

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Homepage from "./pages/Homepage";
 import MakeupShopPage from "./pages/MakeupShop";
 import MakeupProductPage from "./pages/MakeupShop/MakeupProduct";
 import FragranceShopPage from "./pages/FragranceShop";
+import FragranceProductPage from "./pages/FragranceShop/FragranceProduct";
 import CareerPage from "./pages/Careers";
 
 import { Switch, Route } from 'react-router-dom';
@@ -17,10 +18,10 @@ function App() {
 
         <Switch>
           <Route path="/" exact component={Homepage}/>
-          <Route path="/fragrance" component={FragranceShopPage} />
+          <Route path="/fragrance" exact component={FragranceShopPage} />
+          <Route path="/fragrance/product" component={FragranceProductPage} />
 
           <Route path="/makeup" exact component={MakeupShopPage} />
-          {/* The page below is to the render the component with the id from the product choosen */}
           <Route path="/makeup/product" component={MakeupProductPage}/>
 
           <Route path="/careers" component={CareerPage} />
