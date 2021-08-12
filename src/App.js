194fcf88f335f5b9684f265ce84f76e372c29a54
Carlos R Lotus <carlosr_juniorpro@protonmail.com>
2021-08-12 14:33:13 -0300

    Added Page Route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import MakeupShopPage from "./pages/MakeupShop";
 import MakeupProductPage from "./pages/MakeupShop/MakeupProduct";
 import FragranceShopPage from "./pages/FragranceShop";
 import FragranceProductPage from "./pages/FragranceShop/FragranceProduct";
+import CartPage from "./pages/Cart";
 import CareerPage from "./pages/Careers";
 
 import { Switch, Route } from 'react-router-dom';
@@ -18,11 +19,14 @@ function App() {
 
         <Switch>
           <Route path="/" exact component={Homepage}/>
+
           <Route path="/fragrance" exact component={FragranceShopPage} />
           <Route path="/fragrance/product" component={FragranceProductPage} />
 
           <Route path="/makeup" exact component={MakeupShopPage} />
           <Route path="/makeup/product" component={MakeupProductPage}/>
+
+          <Route path="/cart" component={CartPage} />
 
           <Route path="/careers" component={CareerPage} />
         </Switch>
