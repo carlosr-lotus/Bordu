975dcc0d5d7f50569defe4b974b1344f281b12d6
Carlos R Lotus <carlosr_juniorpro@protonmail.com>
2021-08-12 16:56:37 -0300

    Cart Component

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,67 +1,68 @@
+import { useSelector } from 'react-redux';
 import { MdAttachMoney } from 'react-icons/md';
 import { FaTrashAlt } from 'react-icons/fa';
+import { useDispatch } from 'react-redux';
 import { useState } from 'react';
 
 import './Cart.css';
 
 export default function Cart(props) {
-    
+
+    const [shippingTax, setShippingTax] = useState(false);
+    const dispatch = useDispatch();
+
+    // Get array from Reducer
+    const cartList = useSelector(state => state.cart);
+
     // Get props data containing the selected product data
     let productData = props.data;
 
-    // Final price to pay
+    // Create variable and pass the sum of all the prices inside the array passed from reducer
     let finalPrice = 0.00;
-    // if (props) {
-    //     finalPrice += 
-    // }
+    finalPrice = cartList.reduce((prevValue, currentValue) => {return prevValue + currentValue.price}, 0);
 
-    console.log("Props passed to Cart component:");
-    console.log(productData);
-
-    // =-=-=-=-=-=-=-=-=-==-=-=-=-=--=-=-=-=-=-=-=
-    // To Do:
-    // 1) Pass the product data so it can stored in the cart
-    // 2) Create the cart functions such as 'DELETE' item
-    // 3) Make it responsive
-    
-    // Dont worry... you got this. Learn Redux and understand it!
-
-
-    const [shippingTax, setShippingTax] = useState(false);
+    if (shippingTax) {
+        finalPrice += 9.99;
+    }
 
     return (
         <div className="cart-global-container">
 
-            {productData.map((data) => (
-                <div className="cart-local-container" key={data.id}>
-                    <FaTrashAlt className="delete-cart" size={15}/>
+            {productData.map((data) => {
+                
+                return (
+                    <div className="cart-local-container" key={data.id}>
 
-                    <div className="cart-product-details">
-                        <h2>Item</h2>
-                        <img src={data.img} alt="card-product"/>
-                        <h1>{data.title}</h1>
-                    </div>
+                        <FaTrashAlt className="delete-cart" size={15} onClick={() => dispatch({type: 'REMOVE-CART', payload: data})}/>
 
-                    <div className="cart-price-details">
-                        <div className="cart-product-price">
-                            <h2>Preço</h2>
-                            <strong>R$ {data.price}</strong>
+                        <div className="cart-product-details">
+                            <h2>Item</h2>
+                            <img src={data.img} alt="card-product"/>
+                            <h1>{data.title}</h1>
                         </div>
 
-                        {/* Shipping Tax Input */}
-                        <div className="shipping-container">
-                            <label htmlFor="shipping">Frete:</label>
-                            <input type="text" id="shipping" name="shipping" placeholder="20209-383" required />
+                        <div className="cart-price-details">
+                            <div className="cart-product-price">
+                                <h2>Preço</h2>
+                                <strong>R$ {data.price.toFixed(2).replace(".", ",")}</strong>
+                            </div>
 
-                            <button className="red-button shipping-btn" onClick={() => setShippingTax(true)}>
-                                <p>Confirmar Frete</p>
-                            </button>
-
-                            {shippingTax ? <p className="price-shipping">Taxa entrega: R$ 9,99</p> : null}
+                            
                         </div>
                     </div>
-                </div>
-            ))}
+            )})}
+
+            {/* Shipping Tax Input */}
+            {cartList.length > 0 ? <div className="shipping-container">
+                <label htmlFor="shipping">Frete:</label>
+                <input type="text" id="shipping" name="shipping" placeholder="20209-383" required />
+
+                <button className="red-button shipping-btn" onClick={() => setShippingTax(true)}>
+                    <p>Confirmar Frete</p>
+                </button>
+
+                {shippingTax ? <p className="price-shipping">Taxa entrega: R$ 9,99</p> : null}
+            </div> : null}
           
             {/* Final Price */}
             <div className="cart-final-price-container">
